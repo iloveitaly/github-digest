b42d8f27e9c0d9fe9c76d2a3f0cc5176e49d4806
Michael Bianco <iloveitaly@gmail.com>
2024-08-08 13:43:30 -0600

    basic digest working

--- digest.py
+++ digest.py
@@ -1,35 +1,29 @@
 import logging
 from collections import defaultdict
-from datetime import datetime, timedelta
+from datetime import datetime
 
-import requests
+from github import Github
+from github.GithubException import GithubException
 
 from github_digest.email import send_email_markdown
 
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
 def get_github_notifications(since_date: datetime, token: str, repo_ids=None):
-    url = f"https://api.github.com/notifications?since={since_date.isoformat()}"
-    headers = {"Authorization": f"token {token}"}
-    response = requests.get(url, headers=headers)
-    notifications = response.json()
-    if repo_ids:
-        notifications = [n for n in notifications if n["repository"]["id"] in repo_ids]
+    g = Github(token)
+    notifications = []
+    for notification in g.get_user().get_notifications(since=since_date):
+        if not repo_ids or notification.repository.id in repo_ids:
+            notifications.append(notification)
     return notifications
 
 
 def enhance_pr(notification, token):
-    pr_url = notification["subject"]["url"]
-    headers = {"Authorization": f"token {token}"}
-    response = requests.get(pr_url, headers=headers)
-    response.raise_for_status()  # Raise exception if the request failed
-    pr_detail = response.json()
-    # Merge the PR detail with the existing notification data
-    enhanced_notification = {**notification, "pr_detail": pr_detail}
-    return enhanced_notification
+    g = Github(token)
+    pr = g.get_repo(notification.repository.full_name).get_pull(notification.subject.id)
+    return pr
 
 
 def group_notifications_by_repo(notifications, author, token):
@@ -39,17 +33,15 @@ def group_notifications_by_repo(notifications, author, token):
     }
 
     for notification in notifications:
-        repo_name = notification["repository"]["full_name"]
-        if notification["subject"]["type"] == "Issue":
+        repo_name = notification.repository.full_name
+        if notification.subject.type == "Issue":
             grouped["issues"][repo_name].append(notification)
-        elif notification["subject"]["type"] == "PullRequest":
-            notification = enhance_pr(notification, token)
-
-            if notification["pr_detail"]["user"]["login"] == author:
-                grouped["prs"][repo_name].append(notification)
+        elif notification.subject.type == "PullRequest":
+            pr = enhance_pr(notification, token)
+            if pr.user.login == author:
+                grouped["prs"][repo_name].append(pr.raw_data)
             else:
-                # TODO maybe have a separate category for comments on PRs?
-                grouped["issues"][repo_name].append(notification)
+                grouped["issues"][repo_name].append(notification.raw_data)
 
     return grouped
 
@@ -57,43 +49,22 @@ def group_notifications_by_repo(notifications, author, token):
 def format_notifications(grouped_notifications):
     markdown_text = ""
     for repo, notifications in grouped_notifications.items():
-        repo_url = notifications[0]["repository"]["html_url"]
-        markdown_text += f"## [{repo}]({repo_url})\n\n"
-        for notification in notifications:
-            notification_title = notification["subject"]["title"]
-            notification_url = notification["subject"].get("url")
-            if notification_url:
-                notification_url = (
-                    notification_url.replace("api.", "")
-                    .replace("/repos", "")
-                    .replace("pulls", "pull")
+        if notifications:
+            repo_url = notifications[0].get("repository", {}).get("html_url", "#")
+            markdown_text += f"## [{repo}]({repo_url})\n\n"
+            for notification in notifications:
+                notification_title = notification.get("subject", {}).get(
+                    "title", "No title available"
                 )
+                notification_url = notification.get("subject", {}).get("url", "#")
                 markdown_text += f"- [{notification_title}]({notification_url})\n"
-            else:
-                markdown_text += f"- {notification_title} (No URL available)\n"
-        markdown_text += "\n"
+            markdown_text += "\n"
     return markdown_text
 
 
-# TODO integrate as an optional flag
-def mark_notifications_as_read(token, notifications):
-    headers = {"Authorization": f"token {token}"}
-    for notification in notifications:
-        notification_id = notification["id"]
-        url = f"https://api.github.com/notifications/threads/{notification_id}"
-        response = requests.patch(url, headers=headers)
-        if response.status_code != 205:
-            logging.error(
-                f"Failed to mark notification {notification_id} as read. Status code: {response.status_code}"
-            )
-
-
 def get_github_user(token: str):
-    url = "https://api.github.com/user"
-    headers = {"Authorization": f"token {token}"}
-    response = requests.get(url, headers=headers)
-    user_data = response.json()
-    return user_data.get("login", None)
+    g = Github(token)
+    return g.get_user().login
 
 
 def main(
@@ -113,9 +84,9 @@ def main(
 
     print(formatted_text)
 
+    # Uncomment and use the below code to send an email, if needed.
     # if email_auth:
     #     subject_formatted = f"GitHub Digest {since_date.strftime('%m/%d/%Y')}-{datetime.now().strftime('%m/%d/%Y')}"
-
     #     send_email_markdown(
     #         markdown_content=formatted_text,
     #         subject=subject_formatted,
---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from todoist_digest import main
+
+last_synced = None
+
+
+def get_initial_start_date():
+    return (datetime.utcnow() - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def job():
+    global last_synced
+
+    print(f"Running job with last_synced: {last_synced}")
+
+    main(
+        last_synced,
+        os.environ.get("TARGET_USER"),
+        os.environ.get("TARGET_PROJECT"),
+        os.environ.get("EMAIL_AUTH"),
+        os.environ.get("EMAIL_TO"),
+    )
+
+    last_synced = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def cron():
+    global last_synced
+    last_synced = get_initial_start_date()
+
+    schedule = os.environ.get("SCHEDULE", "0 6 * * *")
+
+    print(f"Running on schedule: {schedule}")
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job, CronTrigger.from_crontab(schedule))
+    scheduler.start()
+
+
+if __name__ == "__main__":
+    cron()
