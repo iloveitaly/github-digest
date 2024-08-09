b498b7505a37ae455e7f86fb4fcd29350253680c
Michael Bianco <iloveitaly@gmail.com>
2024-08-09 09:13:32 -0600

    svg properly rendering in email

--- github_digest/svg.py
+++ github_digest/svg.py
@@ -4,14 +4,19 @@ import requests
 from cairosvg import svg2png
 
 
+# https://jsfiddle.net/3kjfLog6/
 # http://stylecampaign.com/blog/2014/01/basics-of-svg-in-email/
 # https://stackoverflow.com/questions/37753911/how-can-i-embed-svg-into-html-in-an-email-so-that-its-visible-in-most-all-emai
-def svg_url_to_base64_png(svg_url, width=32, height=32, dpi=150):
+def svg_url_to_base64_png(svg_url, width=32, height=32, dpi=300):
     response = requests.get(svg_url)
     response.raise_for_status()
+
     png_data = svg2png(
-        bytestring=response.content, output_width=width, output_height=height, dpi=dpi
+        bytestring=response.content,
+        output_width=width,
+        output_height=height,
     )
+
     return f"data:image/png;base64,{base64.b64encode(png_data).decode('utf-8')}"
 
 
