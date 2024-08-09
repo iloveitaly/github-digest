260095f5d201d05c5e86d0b340a7c125c065fea6
Michael Bianco <iloveitaly@gmail.com>
2024-08-09 07:39:21 -0600

    patching is working!

--- github_digest/patch.py
+++ github_digest/patch.py
@@ -4,18 +4,21 @@ https://github.com/nedbat/dinghy/blob/9d8136983a566304793e3e773c97e1ee5f9ae74c/s
 
 import colorsys
 import datetime
+import inspect
 from pathlib import Path
 
 import aiofiles
 import dinghy.jinja_helpers
 import emoji
 import jinja2
-
-from github_digest.svg import svg_url_to_base64_png
+import patchy
+from dinghy.jinja_helpers import datetime_format, label_color_css
 
 
 def custom_render_jinja(template_filename, **variables):
     """Render a template file, with variables."""
+    from github_digest.svg import svg_url_to_base64_png
+
     jenv = jinja2.Environment(
         loader=jinja2.FileSystemLoader(
             [
@@ -25,8 +28,8 @@ def custom_render_jinja(template_filename, **variables):
         ),
         autoescape=True,
     )
-    jenv.filters["datetime"] = dinghy.jinja_helpers.datetime_format
-    jenv.filters["label_color_css"] = dinghy.jinja_helpers.label_color_css
+    jenv.filters["datetime"] = datetime_format
+    jenv.filters["label_color_css"] = label_color_css
 
     # NOTE this is the main addition!
     jenv.globals["inline_svg"] = svg_url_to_base64_png
@@ -38,4 +41,35 @@ def custom_render_jinja(template_filename, **variables):
 
 
 # Monkey patch the original function
-dinghy.jinja_helpers = custom_render_jinja
+# dinghy.jinja_helpers = custom_render_jinja
+
+
+def patch_me():
+    print("unpatched")
+
+
+def patched():
+    print("patched")
+
+
+import re
+
+
+def replace_first_method_name(code, new_name):
+    return re.sub(r"^def\s+\w+\(", f"def {new_name}(", code, count=1)
+
+
+patchy.replace(
+    patch_me,
+    inspect.getsource(patch_me),
+    replace_first_method_name(inspect.getsource(patched), patch_me.__name__),
+)
+
+patchy.replace(
+    dinghy.jinja_helpers.render_jinja,
+    inspect.getsource(dinghy.jinja_helpers.render_jinja),
+    replace_first_method_name(
+        inspect.getsource(custom_render_jinja),
+        dinghy.jinja_helpers.render_jinja.__name__,
+    ),
+)
