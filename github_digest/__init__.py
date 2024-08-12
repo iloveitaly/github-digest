cdf6ca42aa488f36df9d2b6204e19e16661d9ae9
Michael Bianco <iloveitaly@gmail.com>
2024-08-12 10:39:59 -0600

    fix: add cli tool

--- github_digest/__init__.py
+++ github_digest/__init__.py
@@ -1,12 +1,14 @@
 import asyncio
+import datetime
 from pathlib import Path
 
+import click
 from bs4 import BeautifulSoup
 from decouple import config
 from dinghy.digest import make_digests_from_config
 
 # IMPORTANT!
-import github_digest.patch
+import github_digest.patch as _
 from github_digest.email import send_email
 
 ROOT_DIRECTORY = Path(__file__).parent.parent.resolve()
@@ -14,14 +16,16 @@ DIGEST_TARGET = ROOT_DIRECTORY / "data/digest.html"
 DIGEST_YAML = ROOT_DIRECTORY / "data/digest.yaml"
 
 
-def extract_title(html_file_path):
+def extract_title(html_file_path) -> str | None:
     html_content = html_file_path.read_text()
     soup = BeautifulSoup(html_content, features="html.parser")
     title_tag = soup.title
     return title_tag.string.strip() if title_tag else None
 
 
-def main(since):
+def main(since, email_to, email_auth):
+    print("creating digest")
+
     asyncio.run(make_digests_from_config(str(DIGEST_YAML)))
 
     title = extract_title(DIGEST_TARGET)
@@ -29,10 +33,24 @@ def main(since):
     send_email(
         html_content=DIGEST_TARGET.read_text(),
         subject=title,
-        email_to="iloveitaly@gmail.com",
-        email_auth=config("EMAIL_AUTH", cast=str),
+        email_to=email_to,
+        email_auth=email_auth,
     )
 
+    # TODO can't hurt to leave the file around, right?
     # DIGEST_TARGET.unlink()
 
     print("digest creation complete")
+
+
+@click.command(context_settings={"auto_envvar_prefix": "GITHUB_DIGEST"})
+@click.option(
+    "--since",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=str(datetime.date.today() - datetime.timedelta(days=3)),
+    help="Date to pull notifications since in MM/DD/YYYY format",
+)
+@click.option("--email-to", help="Who to send the digest to", required=True)
+@click.option("--email-auth", help="Email authentication string", required=True)
+def cli(since, email_to, email_auth):
+    main(since, email_to, email_auth)
